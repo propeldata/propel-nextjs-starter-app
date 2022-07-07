978731e6752866027b9f277992d35927d7f11994
Dr. Derek Austin <derekraustin@gmail.com>
2022-07-07 11:44:48 -0500

    chore: run ESLint after Prettier to remove space before function parentheses via VS Code workspace settings
    ```
    /**
     * Note: This repo requires ESLint to be run after Prettier.
     * (The reason we need to run ESLint 2nd is the space-before-func-parens rule.)
     * To do so in VS Code, you need to use an extra extension.
     *
     * Reference: https://github.com/rohit-gohri/vscode-format-code-action
     *
     * Step 1: Make sure you've installed the VS Code Prettier & ESLint extensions:
     *    https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
     *    https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
     * Step 2: On the command line, install the extra extension:
     *         code --install-extension rohit-gohri.format-code-action
     * Step 3: Use either File -> Open Workspace from File... or the command line:
     *         code sample-app-nextjs.code-workspace
     * Step 4: You should now be set up correctly using the local workspace file.
     */
    ```

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,3 +1,20 @@
+/**
+ * Note: This repo requires ESLint to be run after Prettier.
+ * (The reason we need to run ESLint 2nd is the space-before-func-parens rule.)
+ * To do so in VS Code, you need to use an extra extension.
+ *
+ * Reference: https://github.com/rohit-gohri/vscode-format-code-action
+ *
+ * Step 1: Make sure you've installed the VS Code Prettier & ESLint extensions:
+ *    https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode
+ *    https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint
+ * Step 2: On the command line, install the extra extension:
+ *         code --install-extension rohit-gohri.format-code-action
+ * Step 3: Use either File -> Open Workspace from File... or the command line:
+ *         code sample-app-nextjs.code-workspace
+ * Step 4: You should now be set up correctly using the local workspace file.
+ */
+
 module.exports = {
   semi: false,
 }
